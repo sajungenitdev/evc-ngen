67df49be15ab0776af38c1390e9494862be62e26
Szamansaju <szamansaju@gmail.com>
2026-08-22 16:53:05 +0600

    all files are uploaded done

--- app/(main)/ev-chargers/page.tsx
+++ app/(main)/ev-chargers/page.tsx
@@ -1,6 +1,6 @@
 // app/(main)/ev-chargers/page.tsx
-export const dynamic = 'force-dynamic';
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useState, useMemo, useEffect } from 'react';
 import Link from 'next/link';
