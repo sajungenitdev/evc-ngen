67e9eba600aa55e3062c6abab312b3dee959d19d
Szamansaju <szamansaju@gmail.com>
2026-08-22 16:59:17 +0600

    all files are uploaded done

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    // ✅ গুরুত্বপূর্ণ: Static export বন্ধ করুন
-    output: 'standalone',
-    
-    images: {
-        domains: ['localhost', 'images.unsplash.com'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
-    },
+  /* config options here */
 };
 
 export default nextConfig;
